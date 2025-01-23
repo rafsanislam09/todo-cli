bf5a3d645fe465f494eeaa65ab815752e9c26c67
rafsan.islam010 <rafsan.islam010@gmail.com>
2025-01-23 12:14:43 +0000

    Enabled Dart SDK Formatter

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -37,20 +37,22 @@ void addTask() {
   if (addedTaskContent.isEmpty) {
     forwardTo("Error: Task cannot be empty!", addTask);
     return;
-  } 
-  
-  try{
+  }
+
+  try {
     //Writing to file
-    if(savedTasksFile.lengthSync() == 0) {
-      savedTasksFile.writeAsStringSync('$addedTaskContent', mode: FileMode.append);
+    if (savedTasksFile.lengthSync() == 0) {
+      savedTasksFile.writeAsStringSync('$addedTaskContent',
+          mode: FileMode.append);
     } else {
-      savedTasksFile.writeAsStringSync('\n$addedTaskContent', mode: FileMode.append);
+      savedTasksFile.writeAsStringSync('\n$addedTaskContent',
+          mode: FileMode.append);
     }
 
     savedTasks.add(addedTaskContent); //Saving in the memory (RAM)
     forwardTo("Task added successfully", main);
     return;
-  } catch(exception) {
+  } catch (exception) {
     forwardTo("Error while saving added task to disk: $exception", main);
     return;
   }
@@ -65,7 +67,9 @@ void editTask() {
 
   int? editedTaskIndex = int.tryParse(taskToEdit);
 
-  if (editedTaskIndex == null || editedTaskIndex < 1 || editedTaskIndex > savedTasks.length) {
+  if (editedTaskIndex == null ||
+      editedTaskIndex < 1 ||
+      editedTaskIndex > savedTasks.length) {
     forwardTo("Error: Invalid task number!", editTask);
     return;
   }
@@ -85,11 +89,11 @@ void editTask() {
   savedTasks[editedTaskIndex - 1] = editedTaskContent;
 
   //Writing to file
-  try{
+  try {
     savedTasksFile.writeAsStringSync(savedTasks.join('\n'));
     forwardTo("Task edited successfully", main);
     return;
-  } catch(exception) {
+  } catch (exception) {
     forwardTo("Error while saving edit to disk: $exception", main);
     return;
   }
@@ -97,39 +101,43 @@ void editTask() {
 
 void deleteTask() {
   displayTasks();
-  String tasksToDeleteInput = getInput("Enter task number to delete(comma or space-separated):   ");
+  String tasksToDeleteInput =
+      getInput("Enter task number to delete(comma or space-separated):   ");
   checkExit(tasksToDeleteInput);
 
   List<int> deletedTaskIndex = [];
   try {
     deletedTaskIndex = tasksToDeleteInput
-    .split(RegExp(r'[,\s]+'))  //Splits by comma or any whitespace
-    .map((element) => int.parse(element.trim()))
-    .toList();
-  } catch(exception) {
+        .split(RegExp(r'[,\s]+')) //Splits by comma or any whitespace
+        .map((element) => int.parse(element.trim()))
+        .toList();
+  } catch (exception) {
     forwardTo("Error: Invalid task number entered!", deleteTask);
   }
 
   if (deletedTaskIndex.contains(null)) {
-    forwardTo("Error: Invalid input! Please enter valid task number(s).", deleteTask);
+    forwardTo(
+        "Error: Invalid input! Please enter valid task number(s).", deleteTask);
     return;
   }
 
   //Sorting `List<int?>deletedTaskIndex` in descending order to avoid index shifting issues.
   deletedTaskIndex.sort((a, b) => a.compareTo(b));
 
-  for(int index in deletedTaskIndex) {
-    if(index < 1 || index > savedTasks.length) {
+  for (int index in deletedTaskIndex) {
+    if (index < 1 || index > savedTasks.length) {
       forwardTo("Error: Task number $index is out of range!", deleteTask);
     }
   }
 
-  for(int index in deletedTaskIndex) {
+  for (int index in deletedTaskIndex) {
     // `deletedTaskIndex - 1` because dart counts list index from 0 instead of 1, so subtracting 1 is necessary
     savedTasks.removeAt(index - 1);
   }
-  
-  saveTasksToFile(successMessage: "Task(s) has been deleted successfully", errorMessage: "Task could not be deleted");
+
+  saveTasksToFile(
+      successMessage: "Task(s) has been deleted successfully",
+      errorMessage: "Task could not be deleted");
 }
 
 //This function actually helps the user when a user accidentally types another command instead of exit or changes his mind. It helps him to exit from other instances, such as `addTask()` or `deleteTask()`.
@@ -141,14 +149,15 @@ void checkExit(String command) {
 }
 
 //Just a simple ANSI code function. It will clear the console.
-void clearConsole() => print(
-    '\x1B[2J\x1B[0;0H'); //An ANSI code which clears the console a.k.a terminal.
+//An ANSI code which clears the console a.k.a terminal.
+void clearConsole() => print('\x1B[2J\x1B[0;0H');
 
 String getInput(String prompt) {
   stdout.write(prompt);
   String? input = stdin.readLineSync();
   return input ??
-      "Error: Invalid input! Perhaps you left it empty or typed something incorrect."; //It just returns the second value if it is null.
+      "Error: Invalid input! Perhaps you left it empty or typed something incorrect.";
+  //It just returns the second value if it is null.
 }
 
 //This function is just to maintain DRY. I had to write this code block multiple times, so I have created a function.
@@ -160,14 +169,14 @@ void forwardTo(String output, Function forwardedFunction) {
 }
 
 void loadTasks() {
-  try{
-    if(savedTasksFile.existsSync()) {
+  try {
+    if (savedTasksFile.existsSync()) {
       savedTasks = savedTasksFile.readAsLinesSync();
     } else {
       savedTasksFile.createSync();
       savedTasks = [];
     }
-  } catch(exception) {
+  } catch (exception) {
     forwardTo("Error: $exception", exit(1));
   }
 }
@@ -181,12 +190,13 @@ void displayTasks() {
 }
 
 //Saves the tasks to the `savedTasks.txt` file
-void saveTasksToFile({required String successMessage, required String errorMessage}) {
+void saveTasksToFile(
+    {required String successMessage, required String errorMessage}) {
   try {
     savedTasksFile.writeAsStringSync(savedTasks.join('\n'));
     forwardTo(successMessage, main);
     return;
-  } catch(exception) {
+  } catch (exception) {
     forwardTo("$errorMessage: Error: $exception", main);
     return;
   }
